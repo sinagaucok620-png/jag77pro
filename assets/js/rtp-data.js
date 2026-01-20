5c590dd18d1de1e1f6b510ed5a9eb49cffaa6719
sinagaucok620-png <sinagaucok620@gmail.com>
2026-01-20 20:05:27 +0700

    Add files via upload

---
+++ assets/js/rtp-data.js
@@ -0,0 +1,78 @@
+// File: assets/js/rtp-data.js
+// DATABASE RTP LENGKAP - JAGUAR77
+
+const rtpDatabase = [
+    // --- 1. PRAGMATIC PLAY (Raja Slot) ---
+    { nama: "Gates of Olympus", provider: "Pragmatic Play", rtp: 98.8, img: "gates_of_olympus.jpg" },
+    { nama: "Starlight Princess", provider: "Pragmatic Play", rtp: 97.5, img: "starlight_princess.jpg" },
+    { nama: "Sweet Bonanza", provider: "Pragmatic Play", rtp: 89.2, img: "sweet_bonanza.jpg" },
+    { nama: "Aztec Gems", provider: "Pragmatic Play", rtp: 68.5, img: "aztec_gems.jpg" }, // Sedang dingin
+    { nama: "Gates of Gatotkaca", provider: "Pragmatic Play", rtp: 95.1, img: "gates_of_gatotkaca.jpg" },
+    { nama: "Sugar Rush", provider: "Pragmatic Play", rtp: 96.0, img: "sugar_rush.jpg" },
+    { nama: "Starlight Princess 1000", provider: "Pragmatic Play", rtp: 98.2, img: "starlight_princess_1000.jpg" },
+    { nama: "Gates of Olympus 1000", provider: "Pragmatic Play", rtp: 99.1, img: "gates_of_olympus_1000.jpg" },
+    { nama: "Wild West Gold", provider: "Pragmatic Play", rtp: 94.5, img: "wild_west_gold.jpg" },
+    { nama: "Bonanza Gold", provider: "Pragmatic Play", rtp: 92.0, img: "bonanza_gold.jpg" },
+    { nama: "Joker's Jewels", provider: "Pragmatic Play", rtp: 75.0, img: "jokers_jewels.jpg" },
+    { nama: "Pyramid Bonanza", provider: "Pragmatic Play", rtp: 88.0, img: "pyramid_bonanza.jpg" },
+    { nama: "Power of Ninja", provider: "Pragmatic Play", rtp: 91.5, img: "power_of_ninja.jpg" },
+    { nama: "Madame Destiny Megaways", provider: "Pragmatic Play", rtp: 86.5, img: "madame_destiny.jpg" },
+    { nama: "The Dog House Megaways", provider: "Pragmatic Play", rtp: 93.8, img: "dog_house_megaways.jpg" },
+
+    // --- 2. PG SOFT (Favorit Anak Muda) ---
+    { nama: "Mahjong Ways 1", provider: "PG Soft", rtp: 99.5, img: "mahjong_ways.jpg" },
+    { nama: "Mahjong Ways 2", provider: "PG Soft", rtp: 98.9, img: "mahjong_ways_2.jpg" },
+    { nama: "Lucky Neko", provider: "PG Soft", rtp: 97.0, img: "lucky_neko.jpg" },
+    { nama: "Treasures of Aztec", provider: "PG Soft", rtp: 95.5, img: "treasures_of_aztec.jpg" },
+    { nama: "Wild Bandito", provider: "PG Soft", rtp: 93.2, img: "wild_bandito.jpg" },
+    { nama: "Dreams of Macau", provider: "PG Soft", rtp: 88.5, img: "dreams_of_macau.jpg" },
+    { nama: "Caishen Wins", provider: "PG Soft", rtp: 92.0, img: "caishen_wins.jpg" },
+    { nama: "Ganesha Fortune", provider: "PG Soft", rtp: 90.0, img: "ganesha_fortune.jpg" },
+    { nama: "Ways of the Qilin", provider: "PG Soft", rtp: 94.0, img: "ways_of_the_qilin.jpg" },
+    { nama: "Dragon Hatch", provider: "PG Soft", rtp: 76.0, img: "dragon_hatch.jpg" },
+    { nama: "Fortune Rabbit", provider: "PG Soft", rtp: 96.0, img: "fortune_rabbit.jpg" },
+    { nama: "Fortune Ox", provider: "PG Soft", rtp: 91.5, img: "fortune_ox.jpg" },
+    { nama: "The Great Icescape", provider: "PG Soft", rtp: 85.0, img: "great_icescape.jpg" },
+    { nama: "Crypto Gold", provider: "PG Soft", rtp: 90.8, img: "crypto_gold.jpg" },
+
+    // --- 3. HABANERO (Legenda Koi) ---
+    { nama: "Koi Gate", provider: "Habanero", rtp: 98.5, img: "koi_gate.jpg" },
+    { nama: "Hot Hot Fruit", provider: "Habanero", rtp: 94.2, img: "hot_hot_fruit.jpg" },
+    { nama: "Fa Cai Shen", provider: "Habanero", rtp: 89.0, img: "fa_cai_shen.jpg" },
+    { nama: "Wealth Inn", provider: "Habanero", rtp: 92.5, img: "wealth_inn.jpg" },
+    { nama: "Mystic Fortune Deluxe", provider: "Habanero", rtp: 96.0, img: "mystic_fortune.jpg" },
+    { nama: "5 Lucky Lions", provider: "Habanero", rtp: 88.0, img: "5_lucky_lions.jpg" },
+    { nama: "Four Divine Beasts", provider: "Habanero", rtp: 85.0, img: "four_divine_beasts.jpg" },
+    { nama: "Lantern Luck", provider: "Habanero", rtp: 91.0, img: "lantern_luck.jpg" },
+
+    // --- 4. MICROGAMING (Klasik) ---
+    { nama: "Ancient Fortunes: Zeus", provider: "Microgaming", rtp: 93.0, img: "ancient_fortunes_zeus.jpg" },
+    { nama: "Lucky Twins Wilds", provider: "Microgaming", rtp: 95.5, img: "lucky_twins_wilds.jpg" },
+    { nama: "Break Away Deluxe", provider: "Microgaming", rtp: 89.0, img: "break_away.jpg" },
+    { nama: "Playboy Gold", provider: "Microgaming", rtp: 87.5, img: "playboy_gold.jpg" },
+    { nama: "Mask of Amun", provider: "Microgaming", rtp: 96.2, img: "mask_of_amun.jpg" },
+
+    // --- 5. SPADEGAMING (Tema Asia) ---
+    { nama: "Brothers Kingdom", provider: "Spadegaming", rtp: 94.0, img: "brothers_kingdom.jpg" },
+    { nama: "Caishen", provider: "Spadegaming", rtp: 91.5, img: "caishen_spade.jpg" },
+    { nama: "Golden Lotus SE", provider: "Spadegaming", rtp: 95.0, img: "golden_lotus.jpg" },
+    { nama: "Fiery Sevens", provider: "Spadegaming", rtp: 88.0, img: "fiery_sevens.jpg" },
+    { nama: "Roma", provider: "Spadegaming", rtp: 97.0, img: "roma_spade.jpg" },
+
+    // --- 6. JOKER GAMING (Joker123) ---
+    { nama: "Roma", provider: "Joker Gaming", rtp: 98.0, img: "roma_joker.jpg" },
+    { nama: "Golden Dragon", provider: "Joker Gaming", rtp: 92.0, img: "golden_dragon.jpg" },
+    { nama: "Sizzling Hot", provider: "Joker Gaming", rtp: 85.0, img: "sizzling_hot.jpg" },
+    { nama: "Lucky God 2", provider: "Joker Gaming", rtp: 90.0, img: "lucky_god_2.jpg" },
+
+    // --- 7. NOLIMIT CITY (Volatilitas Tinggi) ---
+    { nama: "San Quentin", provider: "NoLimit City", rtp: 96.5, img: "san_quentin.jpg" },
+    { nama: "Fire in the Hole", provider: "NoLimit City", rtp: 95.0, img: "fire_in_the_hole.jpg" },
+    { nama: "Mental", provider: "NoLimit City", rtp: 97.0, img: "mental.jpg" },
+    { nama: "Tombstone RIP", provider: "NoLimit City", rtp: 93.5, img: "tombstone_rip.jpg" },
+
+    // --- 8. CQ9 GAMING ---
+    { nama: "Gu Gu Gu 3", provider: "CQ9", rtp: 94.0, img: "gu_gu_gu_3.jpg" },
+    { nama: "Jump High", provider: "CQ9", rtp: 96.0, img: "jump_high.jpg" },
+    { nama: "Good Fortune", provider: "CQ9", rtp: 91.0, img: "good_fortune.jpg" }
+];
